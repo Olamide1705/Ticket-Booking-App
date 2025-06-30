7aebee5ffa1c7759956498d815f05a68d6158821
Olamide1705 <abdullateefnafisat@gmail.com>
2025-06-30 09:25:34 +0100

    Responsive Ticket Booking App
    * Migrated React Project to Vite for faster builds and better development.
    * Add responsive sidebar for mobile
    *Connect Project to Firestore Database
    * Deploy app to Vercel

---
+++ src/ticket/HomePage.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  
+  const registerButton = () => {
+    navigate("/ticket");
+  };
+  return (
+    <>
+    <div className="bg-cover md:bg-center w-full min-h-screen" style={{ backgroundImage: "url('/images/bg.jpg')" }}>
+      <NavBar />
+      <div className="flex flex-col items-center gap-y-[8px] md:gap-y-[2px] pt-[130px] md:pt-[40px] md:pl-[230px]">
+       <h1 className="font-monoton text-[53px] md:text-[62px] text-[rgb(250,250,250)] text-center leading-[140%]">
+          TECHEMBER FEST{" "}
+          <span className="font-orbitron text-[50px] text-center md:text-[62px]">
+            ”25
+          </span>
+        </h1>
+        
+        <p className="font-Road text-[36px] md:text-[46px] text-[rgb(250,250,250)] text-center leading-[100%]">
+          Code! Create!! Conquer!!!
+        </p>
+        
+        <p className="font-Road text-[30px] md:text-[40px] text-[rgb(250,250,250)] text-center leading-[150%]">
+          Join us for an unforgettable experience!
+        </p>
+        
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center gap-[20px] md:gap-[8px] pt-[20px] px-4 md:pl-[260px] ">
+        <p className="font-mochi text-[22px] md:text-[24px] text-[rgb(250,250,250)] text-center">
+          Grab Your Tickets Here
+        </p>
+
+        <button
+          className="cursor-pointer w-full max-w-[100px] md:max-w-[150px] min-h-[44px] md:min-h-[48px] border border-[rgb(36,160,181)] hover:bg-[rgb(1,29,34)] hover:text-white rounded-[8px] text-[16px] md:text-[18px] text-[rgb(36,160,181)]"
+          onClick={registerButton}
+          type="button"
+        >
+          <i className="fa-solid fa-angle-right"></i>
+        </button>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/ticket/NoPage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import error from '../assets/warning.png'
+import { useNavigate } from 'react-router-dom';
+
+const NoPage = () => {
+  const navigate = useNavigate();
+    useEffect(() => {
+    document.body.classList.add("no-page-body");
+    
+  return () => {
+    document.body.classList.remove("no-page-body");
+  };
+},
+  []);
+
+    const goBackHome = () => {
+     navigate("/")
+};
+  return (
+    <>
+    <div className="bg-[rgb(1,146,146)] p-[100px] h-screen">
+    <div className="flex flex-col items-center justify-center pt-[2px] md:pl-[350px] md:pt-[140px] gap-y-[15px]">
+          <img src={error} alt="" className='md:absolute md:ml-[60px] md:mt-[60px] md:left-1 md:top-1/7 max-w-[350px] md:max-w-[500px] h-[400px] rounded-full'/>
+          <div className='flex flex-col items-center justify-center'>
+        <p className='font-Roboto text-[40px] font-bold md:text-[70px] text-white text-center '>PAGE NOT FOUND</p>
+        <p className='font-Roboto text-[19px] md:text-[28px] text-center w-[250px] md:w-[491px]'>We couldn't find the page you were looking for.</p>
+        </div>
+
+   <button
+            className="cursor-pointer w-full max-w-[250px] h-[48px] bg-[rgb(255,215,0)] hover:bg-[rgb(36,160,181)] hover:text-[white] rounded-[25px] text-[18px] text-black font-Roboto"
+            onClick={goBackHome}
+            type="button"
+        >
+            Back To Home
+            </button>
+    </div> 
+    </div>
+   
+    
+    </>
+  )
+}
+
+export default NoPage
